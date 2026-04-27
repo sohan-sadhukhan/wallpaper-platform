e450945d54baa3722d8f8379b886cdbc2722db6d
Sohan Sadhukhan <mrsohansadhukhan@gmail.com>
2026-04-27 13:34:46 +0530

    Update page.tsx

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,9 @@
 import PaginationQuery from "@/components/PaginationQuery";
 import WallpaperHome from "@/components/WallpaperHome";
+import { auth } from "@/lib/auth";
 import prisma from "@/lib/database/dbClient";
 import { Metadata } from "next";
+import { headers } from "next/headers";
 
 export const metadata: Metadata = {
   title: "Home | Wallpaper App",
@@ -17,6 +19,10 @@ type PageProps = {
 const PAGE_SIZE = 10;
 
 const page = async ({ searchParams }: PageProps) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
   const { page } = await searchParams;
   const pageNumber = Math.max(1, Math.floor(Number(page) || 1));
 
@@ -31,12 +37,19 @@ const page = async ({ searchParams }: PageProps) => {
           },
         },
         favorites: {
+          where:
+            session?.user?.id ?
+              {
+                userId: session.user.id,
+              }
+            : {
+                userId: "__no_user__",
+              },
           select: {
             id: true,
           },
         },
       },
-
       orderBy: { createdAt: "desc" },
       take: PAGE_SIZE,
       skip: (pageNumber - 1) * PAGE_SIZE,
